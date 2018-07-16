709fe7919a06dde095d936ac43ec87099cf4c15c
Shelley Zhang <shelleyyz91@gmail.com>
2018-07-16 17:46:05 +1000

    minor changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    # redirect_to user_path(@current_user) if @current_user.present? use this if you want everything to redirect to edit profile while user is logged in
   end
 end
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,7 +6,7 @@ class SessionController < ApplicationController
     user = User.find_by :email => params[:email]
     if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path
+      redirect_to user_path(user)
     else
       flash[:error] = "Invalid email or password"
       redirect_to login_path
